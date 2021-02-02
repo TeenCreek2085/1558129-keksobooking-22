980c6c6c35ff9270c5c6ff956c7a2051ea5b2f23
Artem <art6668@yandex.ru>
2021-02-02 15:07:09 +0700

    Доработал функции

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 'use strict'
 
-// Функция, возвращающая случайное целое число из переданного диапазона включительно
-const getRandomInteger = function (min, max) {
-  if (min < 0 || max < 0 || max <= min) {
+//Функция, проверяющее условие для диапазона
+const isRangeValid = (min, max) => min >= 0 && max >= 0 && !(min >= max);
+
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+const getRandomInteger = (min, max) => {
+  if(!isRangeValid(min, max)) {
     return null;
   }
+  min = Math.ceil(min);
+  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
-getRandomInteger(0, 30);
+getRandomInteger(0.11, 50);
 
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-const getRandomFloat = function (min, max, numbersAfterPoint = 1) {
-  if (min < 0 || max < 0 || max <= min) {
+//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+const getRandomFloat = (min, max, numbersAfterPoint = 1) => {
+  if(!isRangeValid(min, max)) {
     return null;
   }
   const result = Math.random() * (max - min) + min;
